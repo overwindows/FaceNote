755e87428223e8e462b530af14727c463ea39301
overwindows <overwindows@gmail.com>
2019-01-17 12:08:13 +0800

    FaceNote
    FaceNote

--- train_classifier.sh
+++ train_classifier.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/bash
-
-python3 knn_train_classifier.py \
-    --model_dir ~/models/facenet/20190114-154656/ \
-    --train_dir ~/data/facenote/sample/
+python3 knn_train_classifier.py --model_dir ~/models/facenet/20190114-154656/ --train_dir ~/data/facenote/sample/
